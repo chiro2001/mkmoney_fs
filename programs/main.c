a60dde71723afe723c6ef39ea67a8d0b3368ec25
Chiro <chiro2001@163.com>
2021-10-10 16:51:51 +0800

    init

---
+++ programs/main.c
@@ -0,0 +1,3 @@
+//
+// Created by Chiro on 2021/10/10.
+//
---
+++ src/FileType.h
@@ -0,0 +1,14 @@
+// !!! DO NOT MODIFY THIS FILE !!!
+
+#ifndef FILE_TYPE_H
+#define FILE_TYPE_H
+
+typedef enum {
+    REGULAR_FILE,
+    DIRECTORY,
+} FileType;
+
+void listFile(char *name, FileType type);
+
+#endif
+
